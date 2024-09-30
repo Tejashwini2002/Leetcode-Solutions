8b24e33198b227fb90d6e6ae18e4dc56a3862cc0
Tejashwini U Hosur <125350238+Tejashwini2002@users.noreply.github.com>
2024-09-30 22:01:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (27.62%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        /* We will first transpose the matrix and then reverse each row of the transposed matrix and thus the final matrix is our answer */
+        // Step 1: transpose the matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // Step 2: reverse each row of the transposed matrix
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
