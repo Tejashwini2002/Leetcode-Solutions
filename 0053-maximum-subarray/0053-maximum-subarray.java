e913e7d5631e8b7dda0f68b7785794792e8400c7
Tejashwini U Hosur <125350238+Tejashwini2002@users.noreply.github.com>
2024-09-30 22:22:58 +0530

    Time: 1 ms (99.41%), Space: 56.9 MB (70.03%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Kadane's algorithm
+        int n = nums.length;
+        int maxi = Integer.MIN_VALUE; // maximum sum
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum +=nums[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
+        return maxi;
+    }
+}
