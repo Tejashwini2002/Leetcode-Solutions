1720a7b286b70e1ecd8ee1485017a142c78e6570
Tejashwini U Hosur <125350238+Tejashwini2002@users.noreply.github.com>
2024-10-01 15:47:15 +0530

    Time: 0 ms (100%), Space: 41 MB (50.71%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -4,6 +4,7 @@ class Solution {
         List<Integer> ansRow = new ArrayList<>();
         ansRow.add(1); //inserting the 1st element
 
+        /* There is zero-based indexing for both rows and columns here. */
         //calculate the rest of the elements:
         for (int col = 1; col <= rowIndex; col++) {
             ans = ans * ((rowIndex - col)+1);
